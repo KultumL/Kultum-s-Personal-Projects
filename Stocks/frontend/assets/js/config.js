26825671ebb1198b7a8ea7631fec8c1bbe13bb97
KultumL <klhabai@emory.edu>
2026-01-05 17:11:23 -0500

    for deployment

--- Stocks/frontend/assets/js/config.js
+++ Stocks/frontend/assets/js/config.js
@@ -6,6 +6,6 @@ const IS_LOCAL =
 
 const API = IS_LOCAL
   ? "http://127.0.0.1:8000"
-  : "https://YOUR-RENDER-BACKEND.onrender.com"; // <-- replace
+  : "https://stocksmart-fje8.onrender.com"; 
 
 window.API = API;
